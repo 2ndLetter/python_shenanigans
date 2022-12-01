21d1637234fe849df3bf084e3f7eef4dbf322d0e
Brian <b.matthew.chadwick@gmail.com>
2022-12-01 16:49:42 -0500

    Added comments and removed continue statement

--- python_sum_funct.py
+++ python_sum_funct.py
@@ -2,18 +2,29 @@
 
 def my_funct(array):
     """Function finding sum of max consecutive 1s."""
+    # Declare num_list as empty list
     num_list = []
+    # Declare array_sum as zero
     array_sum = 0
+
+    # For Loop for each number in the array
     for num in array:
+        # If statement when the number equals 1
         if num == 1:
-            array_sum = array_sum + 1
+            # Add 1 to array_sum
+            array_sum += 1
+        # Else If statement if the number does NOT equal 1
         elif num != 1:
+            # Declare array_sum as zero to reset the counter
             array_sum = 0
-            continue
+        # Append value of array_sum to the num_list list
         num_list.append(array_sum)
 
-    print(f"Sum of consecutive 1s is {(max(num_list))}")
+    # f-String to show the answer
+    print(f"Sum of max consecutive 1s is {(max(num_list))}")
 
+# Declare my_array within an array
 my_array = [1, 0, 1, 1, 0, 1, 1, 1]
 
+# Call the my_funct function
 my_funct(my_array)
